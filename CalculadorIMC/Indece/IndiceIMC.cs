44c85ab9bd164999c4a372ad2348f56b1f46137e
Marcos José Rocha do Nascimento <marcosjoseDV@hotmail.com>
2023-01-11 11:09:13 -0300

    ok

---
+++ CalculadorIMC/Indece/IndiceIMC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoEMC.Indece
+{
+    internal class IndiceIMC
+    {
+        public IndiceIMC()
+        {
+            Grau3 = "Obesidade grau 3";
+            Grau2 = "Obesidade grau 2";
+            Grau1 = "Obesidade grau 1";
+            Sobpeso = "Excessso de peso";
+            PesoNormal = "Peso normal";
+            AbaixoPeso = "Abaixo do peso normal";      
+        }
+        public string Grau3 { get; set; }
+        public string Grau2 { get; set; }
+        public string Grau1 { get; set; }
+        public string Sobpeso { get; set; }
+        public string PesoNormal { get; set; }
+        public string AbaixoPeso { get; set; }
+    }
+}
---
+++ CalculadorIMC/Program.cs
@@ -0,0 +1,55 @@
+﻿using CalculadoEMC.Indece;
+namespace CarculadorIMC
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Calculado de IMC");
+            Console.WriteLine();
+
+            Console.Write("Digite sua Peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua Altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Resultado(peso, altura);
+        }
+        static void Resultado(double peso, double altura)
+        {
+            var indece = new IndiceIMC();
+            double imc = peso / (altura * altura);
+
+            if (imc > 40)
+            {
+                Console.WriteLine($"Seu IMC = {imc:F2} - {indece.Grau3}");
+            }
+            else if(imc > 35)
+            {
+                Console.WriteLine($"Seu IMC = {imc:F2} - {indece.Grau2}");
+            }
+            else if (imc > 30)
+            {
+                Console.WriteLine($"Seu IMC = {imc:F2} - {indece.Grau1}");
+            }
+            else if (imc > 25)
+            {
+                Console.WriteLine($"Seu IMC = {imc:F2} - {indece.Sobpeso}");
+            }
+            else if (imc > 18.5)
+            {
+                Console.WriteLine($"Seu IMC = {imc:F2} - {indece.PesoNormal}");
+            }
+            else if (imc < 18.5)
+            {
+                Console.WriteLine($"Seu IMC = {imc:F2} - {indece.AbaixoPeso}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
